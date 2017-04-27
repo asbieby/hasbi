56734dc82278524d0e7236cff73c7b23080757a9
Fachrizal Hasbi <asbieby@gmail.com>
2017-04-27 23:08:42 +0700

    commit tes
    tes 123

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]> <!-->
+<html lang="en">
+  <!--
+<![endif]-->
+
+<!-- Mirrored from tidytheme.com/fort/Fort_Preview/Fort_layout_one/image_background/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 19 May 2016 02:33:02 GMT -->
+<head>
+    <!-- TITLE OF SITE --> 
+    <title>Fachrizal Hasbi</title>
+	
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Fort Multipage Landing Page" />
+	<meta name="keywords" content="Fort, parallax, one page, multipage, responsive, landing, html template" />
+	<meta name="author" content="Theme View">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<!-- Favicons -->
+	<link rel="icon" type="image/png" href="images/favicon.png">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+	
+	
+ <!-- CSS Begins
+================================================== -->
+	<!-- Google Font And Custom Font Begins -->
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Exo:300,100,900' rel='stylesheet' type='text/css'>
+
+	<!-- FONT ICONS -->
+	<!-- font-awesome -->
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<!-- IonIcons -->
+	<link href="icon/ionicons/css/ionicons.css" rel="stylesheet">
+	<!-- Elegant Icons -->
+	<link href="icon/elegant-icons/style.css" rel="stylesheet">
+    <!--/ FONT ICONS -->
+
+	<!--Animate Effect-->
+	<link href="css/animate.css" rel="stylesheet">
+ 	<link href="css/hover.css" rel="stylesheet" >
+	
+	<!--lightbox-->
+	<link href="css/lightbox.css" rel="stylesheet"> 
+
+	<!--Owl Carousel -->
+	<link href="css/owl.carousel.css" rel="stylesheet">
+	<link href="css/owl.theme.css" rel="stylesheet">
+	<link href="css/owl.transitions.css" rel="stylesheet">
+
+	
+	<!--BootStrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+	<link href="css/normalize.css" rel="stylesheet">
+	
+	<!--Preloader -->
+	<link href="css/preloader.css" rel="stylesheet">
+
+	<!-- Main Style -->
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+	
+	<!--Replace Your Color-->
+	<link rel="stylesheet" href="css/colors/default-color.css">
+	<!--Replace Your Color Ends-->
+
+	<!-- Switcher Styles-->
+	<link rel="stylesheet" id="switcher-css" type="text/css" href="css/switcher.css" media="all" />
+	<!-- END Switcher Styles -->
+
+	
+	<!-- Template Color Demo Examples -->
+	<link rel="alternate stylesheet" type="text/css" href="css/colors/moderate_orange.css" title="alternate" media="all" /><!--Moderate orangeColor-->	
+	<link rel="alternate stylesheet" type="text/css" href="css/colors/green.css" title="next" media="all" /><!--Green Color-->	
+	<link rel="alternate stylesheet" type="text/css" href="css/colors/yellow.css" title="bookmark" media="all" /><!--Yellow Color-->	
+	<!-- END Template Color Demo Examples -->
+	
+</head>
+<body>
+
+<!-- Start: Preloader section
+==========================-->  
+<div id="loader-wrapper">
+	<div id="loader"></div>
+	<div class="loader-section section-left"></div>
+	<div class="loader-section section-right"></div>
+</div>
+<!-- End: Preloader section
+==========================-->
+
+
+<!-- Start: Header Section
+=============================================-->   
+
+
+
+
+<!-- Start: Choose Us  Section 
+==================================================--> 
+
+	  
+
+<!-- Start:Contact Section 
+==================================================-->  
+<section class="contact" id="contact">
+    <div class="container">
+        <!-- Title -->
+         <div class="base-header">
+			<h3><small>contact us </small>have a question?  </h3>
+        </div>
+		<!--/ Title -->
+        <div class="row">
+            <!-- col-lg-6 -->
+            <div class="col-lg-6 col-md-6 contact_left">
+                <!-- contact FORM-->
+                <div class="contactForm">
+                    <!--MAIN FORM-->
+                    <form id="contactForm" name="sentMessage" action="http://tidytheme.com/fort/Fort_Preview/Fort_layout_one/image_background/mailer.php">
+                        <div class="form-group">
+                            <input aria-invalid="false" class="form-control" data-validation-required-message="Please enter your name." id="name" name="name" placeholder="Name* :" required="" type="text">
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" data-validation-required-message="Please enter your email address." name="email" id="email" placeholder="E-mail* :" required="" type="email">
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" data-validation-required-message="Please enter your phone number." name="Subject" id="number" placeholder="Subject* :" required="" type="tel">
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" cols="40" data-validation-required-message="Please enter a message." name="Message" id="message" placeholder="Message* :" required="" rows="3"></textarea>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div id="success"></div>
+						<a href="#" class="btn btn-chos hvr-shutter-in-horizontal" title="submit">Send Mesage</a>
+                    </form>
+					<!--END Main form-->
+                </div>
+				<!-- END contact FORM-->
+            </div>
+			<!--/ .col-lg-6 -->
+			<!-- Address-->
+            <div class="col-lg-6 col-md-6 cont_right">
+                <div class="cont-details">
+                    <h3>Fachrizal Hasbi</h3>
+                    <p>testtt 123</p>
+                    <p><i class="icon_pin_alt"></i>Jln.D.I.Panjaitan, Samarinda - Kaltim</p>
+                    <p><i class="icon_mobile"></i>0853 5003 9922</p>
+                    <p><i class="icon_mail_alt"></i>info@fhasbi.my.id</p>          
+                </div>
+            </div>
+			<!--/.Address-->
+        </div>
+		<!--/.row-->
+    </div>
+	<!--/.container-->
+</section>
+<!-- End:Contact Section 
+==================================================-->   
+
+
+
+	  
+<!-- Start:Footer Section 
+==================================================--> 
+<footer class="footer bgc-dark text-white">
+	<div class="container">
+	  
+	  <div class="copyright text-center">
+		<span class="text-primary">Fachrizal Hasbi</span> © 2017. 
+	  </div> <!--/.copyright-->
+	</div><!--/.container-->
+</footer>
+<!-- End:Footer Section 
+========================================-->
+
+	
+
+<!-- Scripts
+========================================-->
+<!-- jquery -->
+<script src="js/vendor/jquery-1.12.0.min.js" type="text/javascript" ></script>
+<!-- Modernizer -->
+<script src="js/modernizr.custom.html" type="text/javascript"></script>
+<!-- plugins -->
+<script src="js/plugins.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script> 
+<!-- Use For Animation -->
+<script src="js/wow.min.js"></script>
+<!-- Use For Map -->
+<script src="http://maps.google.com/maps/api/js?sensor=false&amp;libraries=geometry&amp;v=3.7"></script>
+<!-- Use For Map -->
+<script src="js/maplace.js"></script>
+<!-- Use For carousel -->
+<script src="js/owl.carousel.min.js"></script>
+<!-- Use For mixitup gallery -->
+<script src="js/jquery.mixitup.min.js"></script>
+<!-- Use For Animated Header -->
+<script src="js/animatedheader.js"></script>
+<!-- Use For Image Peview -->
+<script src="js/lightbox.min.js"></script>
+<!-- Use For single Page Nav -->
+<script src="js/jquery.singlePageNav.min.js"></script>
+<!--fswit Switcher   -->
+<script src="js/fswit.js"></script>
+<!-- Custom Scripts
+========================================-->
+<script src="js/main.js"></script>
+
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs1.uzone.id/cfspushadsv2/request" + "?id=1" + "&enc=telkom2" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582ECSaLdwqSpn6%2bRC2UIDDvTfIkut5k%2fIaDNN3GkDuSrd3pAglNs0hwMKo2kyET6EI34kww%2bPEMgzsncLDz9HMl5RiosEKYpeAC7KTiI7kPVsv5cUJdkNWJzRP0gXfLaULH3TT3B6c%2fJVAK4n4RcA75G7YZOxkMuiTQ0Gr14%2f%2fEg35hwDP31nXVUQ0%2bnyTkI1V9LFnUe7EXY1pua7iR%2bBb95PQlIuq1DgE7cMvaGRgLRFm2q%2fV%2bXBzsOJoOWVj0OPJrfbCTc27BnkR3hFC0k2Hn42wqUgg19OW%2b94UAd6jUL7js2H0F9E%2ffjGP%2fSPuIGrvqKlFOO7nW0PJpiC1%2fsm5O%2fYRDmm%2fQRwAilEvHpwDUfh6LDnxCX4%2bdqAnmmnxVdNa3NS3zFBQuTFHUKTrkyPHMOUeT1KhZn%2f503dLNphEN2f3q17bsOr%2f5fr8L%2bYRR5UbmmFDMJbE2Wh5qit5qgDk%2fwCobF1qGfwv3asTO%2bBWMYsF0Ykvu88%2fRPE2Ysj%2bniJujMM8gmDzQz2ZbKaUt4GUHBzNTYz1gFJFD%2fPCWZRnvyEerCmL9yEWW3LgkGRd0TKfhjG%2bm8weIaLbR6EnvxYEGXFyFrSJ%2fGE3jVjOFNM9ZlwIq26gLn615iRytTKJDhSeg%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+
+<!-- Mirrored from tidytheme.com/fort/Fort_Preview/Fort_layout_one/image_background/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 19 May 2016 02:33:53 GMT -->
+</html>
